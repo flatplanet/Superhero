f2ec22b2ba3556c4d0b92c4a2bd87d4e79c4c9b1
John Elder <johne4196@gmail.com>
2016-09-12 00:35:25 +0000

    added main page functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
   def index
+    @super_hero_name = Faker::Superhero.name
+    @super_hero_power = Faker::Superhero.power
+    
+    
+    @random_set = rand(1..2)
+    if @random_set == 1
+      @random_set_pic = "set1"
+    else
+      @random_set_pic = "set2"
+    end
+    
+    @corporate_job = Faker::Name.title
+    @corporate_power = Faker::Company.bs
+    @random_avatar = rand(1..100)
+    @super_hero_avatar = Faker::Avatar.image(@random_avatar, "150x150", "png", @random_set_pic, "bg2" )
   end
 
   def team
